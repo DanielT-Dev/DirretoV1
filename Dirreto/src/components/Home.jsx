81b69a32c02168938ad78b38a3b36a5c7a5d7062
DanielT-Dev <danieltrusca1@outlook.com>
2024-10-06 17:02:04 +0300

    Updated Projects tab.

--- Dirreto/src/components/Home.jsx
+++ Dirreto/src/components/Home.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import "../styles/Home.css"
 
 import Navigation2 from './Navigation2'
+import Projects from './Projects'
 
 const Home = () => {
   return (
     <div className="home_container">
         <Navigation2/>
+        <Projects/>
     </div>
   )
 }
---
+++ Dirreto/src/components/Projects.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <div className="projects_container">
+            <h1>
+                Search Project
+            </h1>
+        </div>
+        <div className="list_container">
+            <div className="project">
+
+            </div>
+            <div className="project">
+                
+            </div>
+            <div className="project">
+                
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+                
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+                
+            </div>
+            <div className="project">
+                
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
