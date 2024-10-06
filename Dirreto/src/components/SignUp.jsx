a716563a2f71bad55eabeac6718d556aaec1f1dd
DanielT-Dev <danieltrusca1@outlook.com>
2024-10-06 12:43:24 +0300

    Updated Sign-Up component.

--- Dirreto/src/components/SignUp.jsx
+++ Dirreto/src/components/SignUp.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import "../styles/SignUp.css"
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="sign_up_container">
+        <div className="sign_up_form">
+            <h1>
+                Dirreto
+            </h1>
+            <input 
+                type="text"
+                placeholder="Email"
+            />
+            <input 
+                type="password"
+                placeholder="Password"
+            />
+            <button>
+                Sign-Up
+            </button>
+            <p>
+                Already have an account?
+            </p>
+            <p style={{color: "rgb(100, 150, 255)", cursor: "pointer"}}>
+                Go to Log-In
+            </p>
+        </div>
+    </div>
   )
 }
 
