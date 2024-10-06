21d27422942a02d53b7f07677f63f9c95dc0a6fd
DanielT-Dev <danieltrusca1@outlook.com>
2024-10-06 17:30:11 +0300

    Updated Projects component.

--- Dirreto/src/components/Projects.jsx
+++ Dirreto/src/components/Projects.jsx
@@ -7,6 +7,43 @@ const Projects = () => {
             <h1>
                 Search Project
             </h1>
+            <input 
+              type="text"
+              placeholder='Project Name'
+            />
+            <button>
+              <img src="/search1.png"/>
+              Search
+            </button>
+
+            <p>
+              Filter Projects By 
+            </p>
+            <div style={{display: "flex", flexDirection: "row"}}>
+              <button class="projects_button">
+                Public
+              </button>
+              <button class="projects_button">
+                Private
+              </button>
+            </div>
+            <div style={{display: "flex", flexDirection: "row"}}>
+              <button class="projects_button">
+                Newest
+              </button>
+              <button class="projects_button">
+                Oldest
+              </button>
+            </div>
+            <div style={{display: "flex", flexDirection: "row"}}>
+              <button class="projects_button">
+                Best Rating
+              </button>
+              <button class="projects_button">
+                Most Views
+              </button>
+            </div>
+            
         </div>
         <div className="list_container">
             <div className="project">
@@ -14,27 +51,6 @@ const Projects = () => {
             </div>
             <div className="project">
                 
-            </div>
-            <div className="project">
-                
-            </div>
-            <div className="project">
-
-            </div>
-            <div className="project">
-                
-            </div>
-            <div className="project">
-
-            </div>
-            <div className="project">
-                
-            </div>
-            <div className="project">
-                
-            </div>
-            <div className="project">
-
             </div>
             <div className="project">
                 
