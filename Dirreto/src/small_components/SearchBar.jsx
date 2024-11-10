5578a8d3c1cde5a8a6c8b71602c2a1a3c5861baa
DanielT-Dev <danieltrusca1@outlook.com>
2024-11-10 18:40:54 +0200

    Updated search bar UI.

--- Dirreto/src/small_components/SearchBar.jsx
+++ Dirreto/src/small_components/SearchBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import './SearchBar.css'
 
 const SearchBar = () => {
   return (
     <div className='search_bar_container'>
+        <span>
+          CTRL + K
+        </span>
+        <img src="/search2.png"/>
         <input 
             type="text"
+            placeholder="Search project..."
         />
+        
     </div>
   )
 }
